ead197179e151b6be3e3ae631201978d01a1a1b3
johnmorris4 <johnmorris4@bellsouth.net>
2017-08-13 09:19:42 -0400

    added final files

---
+++ server/main.js
@@ -0,0 +1,43 @@
+import { Meteor } from 'meteor/meteor';
+
+Meteor.publish('todos', function todoPublication(){
+  return Todos.find({
+    $or: [
+      {private:{$ne: true}},
+      {owner: this.userId}
+    ]
+  });
+});
+
+Meteor.methods({
+  'todos.insert'(text, time){
+    if(!this.userId){
+      throw new Meteor.Error('Not Authorized');
+    }
+
+    Todos.insert({
+      text,
+      time,
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  'todos.setChecked' (id, setChecked){
+    Todos.update(id, {$set:{checked: setChecked}});
+  },
+  'todos.remove' (id){
+    if (todo.owner !== this.userId){
+      throw new Meteor.Error('Unthorized');
+    }
+    
+    Todos.remove(id);
+  },
+  'todos.setPrivate'(id, setToPrivate){
+    const todo = Todos.findOne(id);
+
+    if (todo.owner !== this.userId){
+      throw new Meteor.Error('Unthorized');
+    }
+    Todos.update(id, {$set:{private: setToPrivate}});
+  }
+});
